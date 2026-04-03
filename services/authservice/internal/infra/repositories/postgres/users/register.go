45ab25807a5e3a4cd7983f4ef9b8a40a1125e415
Dima <dv.tyumentsev@gmail.com>
2026-04-03 14:22:13 +0300

    authservice edit

--- services/authservice/internal/infra/repositories/postgres/users/register.go
+++ services/authservice/internal/infra/repositories/postgres/users/register.go
@@ -2,7 +2,7 @@ package users
 
 import (
 	"Donate_backend/services/authservice/internal/domain/entity"
-	postgres "Donate_backend/services/authservice/internal/infra/repositories/postgres"
+	"Donate_backend/services/authservice/internal/infra/repositories/postgres"
 	"context"
 )
 
@@ -18,9 +18,50 @@ func NewDatabase(pg *postgres.Postgres) *Database {
 
 func (db *Database) Save(ctx context.Context, user *entity.User) error {
 	const op = "users.Save"
-	query := `insert into users(email, password, username) VALUES ($1, $2, $3)`
+	query := `insert into users(email, password_hash, username, created_at) VALUES ($1, $2, $3, $4) if exists update`
 	ctx = db.pg.WriteWithTimeout(ctx)
-	_, err := db.pg.Pool.Exec(ctx, query, user.Email, user.PassHash, user.Username)
+
+	_, err := db.pg.Pool.Exec(ctx, query, user.Email, user.PassHash, user.Username, user.CreatedAt)
+	if err != nil {
+		return postgres.MapError(err, op)
+	}
+
+	return nil
+}
+
+func (db *Database) GetUserByID(ctx context.Context, uuid string) (*entity.User, error) {
+	const op = "users.GetUserByID"
+	query := `select * from users where uuid = $1 returnning email, password_hash, username, created_at`
+	ctx = db.pg.ReadWithTimeout(ctx)
+	var user *entity.User
+
+	err := db.pg.Pool.QueryRow(ctx, query, uuid).Scan(&user)
+	if err != nil {
+		return nil, postgres.MapError(err, op)
+	}
+
+	return user, nil
+}
+
+func (db *Database) UpdateUsername(ctx context.Context, oldUsername, newUsername string) error {
+	const op = "users.UpdateUsername"
+	query := `update users set username = $1 where username = $2`
+	ctx = db.pg.WriteWithTimeout(ctx)
+
+	_, err := db.pg.Pool.Exec(ctx, query, newUsername, oldUsername)
+	if err != nil {
+		return postgres.MapError(err, op)
+	}
+
+	return nil
+}
+
+func (db *Database) DeleteUserByID(ctx context.Context, uuid string) error {
+	const op = "users.DeleteUserByID"
+	query := `delete * from users where uuid = $1`
+	ctx = db.pg.WriteWithTimeout(ctx)
+
+	_, err := db.pg.Pool.Exec(ctx, query, uuid)
 	if err != nil {
 		return postgres.MapError(err, op)
 	}
