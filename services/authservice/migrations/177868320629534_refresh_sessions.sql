32bc8efd154e4523e650033e82e47d365153b1e3
Dima <dv.tyumentsev@gmail.com>
2026-05-13 20:17:40 +0300

    фикс миграций users и refresh_sessions

--- services/authservice/migrations/177868320629534_refresh_sessions.sql
+++ services/authservice/migrations/177868320629534_refresh_sessions.sql
@@ -1,13 +1,14 @@
 -- +goose Up
 create table if not exists refresh_sessions(
     session_id bigserial primary key,
-    refresh_token_hash text not null unique,
+    refresh_token_hash text not null,
     device_id text,
     created_at timestamptz not null default now(),
     expires_at timestamptz not null,
     revoked_at timestamptz,
     replaced_at timestamptz,
-    user_id uuid not null references users(id) on delete cascade
+    user_id uuid not null references users(id) on delete cascade,
+    constraint refresh_sessions_refresh_token_hash_unique unique(refresh_token_hash)
 );
 create index idx_refresh_sessions_user_id on refresh_sessions(user_id);
 create index idx_refresh_session_expires_at on refresh_sessions(expires_at);
