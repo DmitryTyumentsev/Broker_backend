1957628d63495a3184413dc317c64ea90eb1a043
Dima <dv.tyumentsev@gmail.com>
2026-05-15 14:04:52 +0300

    Фикс в миграциях и MapError

--- services/authservice/internal/infra/repositories/postgres/errors.go
+++ services/authservice/internal/infra/repositories/postgres/errors.go
@@ -12,7 +12,9 @@ import (
 )
 
 const (
-	constraintUniqueUsersEmail = "unique_users_email"
+	constraintUniqueEmail            = "unique_users_email"
+	constraintCheckUserRole          = "check_users_user_role" //для самих const лучше писать сокращенное название?
+	constraintUniqueRefreshTokenHash = "unique_refresh_sessions_refresh_token_hash"
 )
 
 func MapError(op string, err error) error {
@@ -22,7 +24,7 @@ func MapError(op string, err error) error {
 
 	switch {
 	case errors.Is(err, pgx.ErrNoRows):
-		return fmt.Errorf("%s: %w", op, domain.ErrNotFound)
+		return fmt.Errorf("%s: %w", op, domain.ErrNotFound) //верно ли это тут оставлять или можно ниже в case добавить?
 	}
 
 	var pgErr *pgconn.PgError
@@ -30,18 +32,25 @@ func MapError(op string, err error) error {
 		switch pgErr.Code {
 		case pgerrcode.UniqueViolation:
 			switch pgErr.ConstraintName {
-			case constraintUniqueUsersEmail:
+			case constraintUniqueEmail:
 				return fmt.Errorf("%s: %w", op, domain.ErrNotUniqueEmail)
-			}
-			return fmt.Errorf("%s: %w", op, domain.ErrNotUnique) //мне немного не нравится switch в switch, ок ли так или можно более читаемо сделать? как на продовых проектах делают? или как щас у меня хорошо читаемо?
-		case pgerrcode.NotNullViolation:
-			return fmt.Errorf("%s: %w", op, domain.ErrMustBeNotNull)
-		case pgerrcode.ForeignKeyViolation:
-			return fmt.Errorf("%s: %w", op, domain.ErrBadRequest)
-		default:
-			return fmt.Errorf("%s: postgres error code=%s: %w", op, pgErr.Code, err)
-		}
-	}
+			case constraintUniqueRefreshTokenHash:
+				return fmt.Errorf("%s: %w", op, domain.ErrNotUniqueRefreshTokenHash) //верно рассуждаю что надо возвращать ошибку в доменный слой чтобы сделать новую попытку создания хэша?
+			} //мне немного не нравится switch в switch, ок ли так или можно более читаемо сделать? как на продовых проектах делают? или как щас у меня хорошо читаемо? и еще вопрос - зачем нужен pgerrcode.IntegrityConstraintViolation?
 
+		case pgerrcode.NotNullViolation:
+			return fmt.Errorf("%s: %w, full error: %w", op, domain.ErrMustBeNotNull, err)
+
+		case pgerrcode.ForeignKeyViolation:
+			return fmt.Errorf("%s: %w, full error: %w", op, domain.ErrBadRequest, err) //внешне получился винергрет внутри самого fmt.Errorf как по мне. Как тебе? или норм?
+
+		case pgerrcode.CheckViolation:
+			switch pgErr.ConstraintName {
+			case constraintCheckUserRole:
+				return fmt.Errorf("%s: %w", op, domain.ErrUserRoleInvalid)
+			}
+		}
+		return fmt.Errorf("%s: postgres error code=%s: %w", op, pgErr.Code, err)
+	}
 	return fmt.Errorf("%s: %w", op, err)
 }
