25dee65e832a1ecd9ef5590a3bb777d0969bb0bd
Dima <dv.tyumentsev@gmail.com>
2026-07-06 14:03:47 +0300

    промежуточное в apigateway 6, разбор вопросов

--- shared/pkg/auth/permissions/permissions.go
+++ shared/pkg/auth/permissions/permissions.go
@@ -4,26 +4,10 @@ const (
 	CustomerFixationCreate = "customer_fixation.create"
 	CustomerFixationRead   = "customer_fixation.read"
 	CustomerFixationCancel = "customer_fixation.cancel"
-
-	BrokerTeamManage = "broker_team.manage"
-
-	ProjectRead   = "project.read"
-	ProjectManage = "project.manage"
-
-	LotRead = "lot.read"
-
-	DealRead   = "deal.read"
-	DealManage = "deal.manage"
 )
 
 var All = []string{
 	CustomerFixationCreate,
 	CustomerFixationRead,
 	CustomerFixationCancel,
-	BrokerTeamManage,
-	ProjectRead,
-	ProjectManage,
-	LotRead,
-	DealRead,
-	DealManage,
 }
